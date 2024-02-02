18687db54fd60795506e114bc39cfc95fff5432b
JJRoca <hanzoblade1234@outlook.com>
2024-02-02 10:52:04 -0400

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { CategoriesComponent } from './pages/admin/categories/categories.component';
+import { CartComponent } from './pages/admin/cart/cart.component';
+import { CustomerComponent } from './pages/admin/customer/customer.component';
+import { OrderComponent } from './pages/admin/order/order.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { LandingComponent } from './pages/website/landing/landing.component';
+import { CategoryProductsComponent } from './pages/website/category-products/category-products.component';
+import { CustomerCartComponent } from './pages/website/customer-cart/customer-cart.component';
+import { CheckoutComponent } from './pages/website/checkout/checkout.component';
+import { CustomerOrdersComponent } from './pages/website/customer-orders/customer-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //admin
+    LoginComponent,
+    LayoutComponent,
+    CategoriesComponent,
+    CartComponent,
+    CustomerComponent,
+    OrderComponent,
+    ProductsComponent,
+    //website
+    LandingComponent,
+    CategoryProductsComponent,
+    CustomerCartComponent,
+    CheckoutComponent,
+    CustomerOrdersComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
